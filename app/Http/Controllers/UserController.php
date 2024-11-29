41907881ec7871985d7411f18ec3c95d65cfdbea
Necrozzyz <necrozzycontact@proton.me>
2024-11-28 21:34:45 -0300

    Configuração UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,16 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    // Obter o usuário autenticado
+    public function getAuthenticatedUser(Request $request)
+    {
+        return response()->json($request->user());
+    }
+    
+    //Listar todos os users (somente para admins)
     public function index()
     {
-        //
+        $usuarios = User::all();
+        return response()->json($usuarios);
     }
 
     /**
@@ -22,20 +29,29 @@ class UserController extends Controller
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+    //Cadastrar novo usuário
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+        
+        $user = User::create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => Hash::make($validated['password']),
+        ]);
+        
+        return response()->json($user, 201);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
+    // Exibir informações de um usuário específico
+    public function show($id)
     {
-        //
+        $usuario = User::findOrFail($id);
+        return response()->json($usuario);
     }
 
     /**
